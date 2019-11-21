838c42c735f999de24fa24f60dba4ad806c679b8
LinWei <946281858@qq.com>
2019-11-22 00:36:58 +0800

    训练公式实现为训练过程

--- exa/neural_network.py
+++ exa/neural_network.py
@@ -15,10 +15,20 @@ class NeuralNetwork:
         self.who = numpy.random.rand(self.o_nodes, self.h_nodes) - 0.5
         self.activation_func = lambda x: scipy.special.expit(x)
 
-    def train(self):
+    def train(self, inputs_list, target_list):
         # 更新节点j与下一层节点k间的链接权重公式为：△W(j,k) = α * E(k) * sigmoid(O(k)) * (1 - sigmoid(O(k))) · O(j).T
         # E(k)为下一节点误差：E(k) = target - sigmoid(O(k))
         # E(j)为更新节点j的误差，可以由E(k)及所连接权重分隔误差，计算出：E(j) = W(j,k).T · E(k)
+        targets = numpy.array(target_list, ndmin=2).T
+        inputs, hidden_output, final_outputs = self.query(inputs_list)
+        # 计算E(k) [最终输出层反馈误差]
+        final_error = targets - final_outputs
+        # 计算E(j) [隐藏层反馈误差]
+        hidden_error = numpy.dot(self.who.T, final_error)
+        # 根据误差更新链接权重 [隐藏层与最终输出层]
+        self.who += numpy.dot(self.lr * final_error * final_outputs * (1.0 - final_outputs), hidden_output.T)
+        # 根据误差更新链接权重 [输入层与隐藏层]
+        self.wih += numpy.dot(self.lr * hidden_error * hidden_output * (1.0 - hidden_output), inputs.T)
         pass
 
     def query(self, inputs_list):
@@ -32,4 +42,4 @@ class NeuralNetwork:
         # 隐藏层到最终输出层同理
         final_inputs = numpy.dot(self.who, hidden_output)
         final_outputs = self.activation_func(final_inputs)
-        return final_outputs
+        return inputs, hidden_output, final_outputs
--- exa/runner.py
+++ exa/runner.py
@@ -8,6 +8,9 @@ def main():
 
     network = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, 0.3)
     print(network.query([1.0, 0.5, -1.5]))
+    for i in range(0, 30):
+        network.train([1.0, 0.5, -1.5], [0.7, 0.8, 0.9])
+    print(network.query([1.0, 0.5, -1.5]))
 
 
 if __name__ == '__main__':
